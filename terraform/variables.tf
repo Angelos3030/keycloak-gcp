2ed462df7b492a6456682ebd43aab81f77544e61
angelos papadopoulos <PapadopoulosAn@piraeusbank.gr>
2026-08-20 05:01:47 +0000

    new structure

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the project in which to create resources."
+  type        = string
+}
+
+variable "region" {
+  description = "The region in which to create resources."
+  type        = string
+}
